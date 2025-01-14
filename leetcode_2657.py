af764fca06b42c3105d6ffd062634766189462e8
ktsyg <ks.tsyganok@gmail.com>
2025-01-14 15:08:31 +0300

    Task #2657

---
+++ leetcode_2657.py
@@ -0,0 +1,25 @@
+def findThePrefixCommonArray(A, B):
+    prefix_common = []
+    dict_nums = {x: 0 for x in range(51)}
+    counter = 0
+    for i in range(len(A)):
+        dict_nums[A[i]] += 1
+        dict_nums[B[i]] += 1
+        if dict_nums[A[i]] == 2 and A[i] != B[i]:
+            counter += 1
+        if dict_nums[B[i]] == 2 and A[i] != B[i]:
+            counter += 1
+        if A[i] == B[i]:
+            counter += 1
+        prefix_common.append(counter)
+    return prefix_common
+
+
+def main():
+    A = [2,3,1]
+    B = [3,1,2]
+    print(findThePrefixCommonArray(A, B))
+
+
+if __name__ == '__main__':
+    main()
