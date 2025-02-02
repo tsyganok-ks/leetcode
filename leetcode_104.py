dd851b30a8cd5598c711e9bdb53af17986a51f14
ktsyg <ks.tsyganok@gmail.com>
2025-02-02 21:09:57 +0300

    Task #104

--- leetcode_104.py
+++ leetcode_104.py
@@ -16,3 +16,30 @@ class Solution:
             return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
         else:
             return 0
+
+    def maxDepth2(self, root: Optional[TreeNode]) -> int:
+        """
+        Alternative version of finding max depth of binary tree
+        :param root:
+        :return:
+        """
+        if root:
+            queue = deque()
+            queue.append((root, 1))
+
+            self.res = 0
+
+            while queue:
+                root, num = queue.popleft()
+
+                if not root.left and not root.right:
+                    self.res = max(self.res, num)
+
+                if root.left:
+                    queue.append((root.left, num + 1))
+                if root.right:
+                    queue.append((root.right, num + 1))
+
+            return self.res
+        else:
+            return 0
